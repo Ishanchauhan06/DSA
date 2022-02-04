28ef5a67fffaae82dc72395adca62db676885012
Ishan <chauhanishan82@gmail.com>
2022-02-04 23:58:16 +0530

    quicksort code algo added

---
+++ quicksort.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int partition(int* arr, int s, int e){
+    int pivot = arr[s];
+    int cnt=0;
+    for (int i = s+1; i<=e; i++)
+    {
+     if(arr[i]<=pivot){
+      cnt++;
+     }   
+    }
+    // pivotindex location
+    int pivotindex = s + cnt;
+    swap(arr[pivotindex],arr[s]);
+    // correcting the position of elementwhich is at left and right of pivot 
+    int i=s;
+    int j = e;
+    while (i<pivotindex && j> pivotindex)
+    {
+      while (arr[i]<=pivot)
+      {
+          i++;
+      }
+      while (arr[j]>pivot)
+      {
+          j--;
+      }  
+    }
+    if(i<pivotindex && j> pivotindex){
+        swap(arr[i++],arr[j--]);
+    }
+   return pivotindex; 
+}
+void quicksort(int* arr, int s, int e ){
+    if(s<e){
+        // cout<<"HI"<<" ";
+    int p = partition(arr,s,e);
+    //left part
+    quicksort(arr,s,p-1);
+    //right part
+    quicksort(arr,p+1,e);
+    }
+}
+int main(){
+   int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    quicksort(arr,0,n-1);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+   
+return 0;
+}
