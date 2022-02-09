f8eed65355f9eb469b3071da3556529d65dc2e66
Ishan <chauhanishan82@gmail.com>
2022-02-09 18:43:00 +0530

    recursive way to reverse a linked list

---
+++ LinkedList/ReverseRecursive.cpp
@@ -0,0 +1,71 @@
+// A complete working C++ program to demonstrate
+// all insertion methods on Linked List
+#include <bits/stdc++.h>
+using namespace std;
+
+// A linked list node
+class Node
+{
+	public:
+	int data;
+	Node *next;
+    Node(int data){
+        this-> data = data;
+        next = NULL;
+    }
+};
+ 
+Node  *takeInput(){
+  int data;
+  cin>> data;
+  Node* head = NULL;
+  Node* tail = NULL;
+  while (data != -1){
+   Node* newNode = new Node(data);
+    if (head == NULL){
+     head = newNode;
+     tail = newNode;
+    }
+    else{
+      tail -> next = newNode;
+      tail = tail ->next;
+    }
+    cin>>data;
+  }
+ 
+   return head;
+}
+
+Node* recursive(Node* &head){
+    if(head==NULL|| head->next == NULL){
+        return head;
+    }
+    Node* smallpart = recursive(head->next);
+    head->next->next = head;
+    head->next = NULL ;
+    return smallpart;
+}
+
+void display(Node* &head){
+     Node* temp = head;
+     while(temp!=NULL){
+         cout<<temp->data<<" ";
+         temp = temp->next;
+     }cout<<endl;
+}
+
+/* Driver code*/
+int main()
+{
+	Node* head = takeInput();
+	cout<<"Current Node is: "<<endl;
+    display(head);
+	Node* newhead = recursive(head);
+    cout<<"recursive list is: "<<endl;
+    display(newhead);
+
+	return 0;
+}
+
+
+// This code is contributed by rathbhupendra, arkajyotibasak
