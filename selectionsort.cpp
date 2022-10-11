d3a34db338460482fb58f10be01fab62de9e0e02
shivamuttpal <72176766+shivamuttpal@users.noreply.github.com>
2022-10-11 19:32:01 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // selection sort based on the technique in which we need to find the smallest element in the array and place it to the correct location
 
-void selection(int arr[], int n)
+void selectionsort(int arr[], int n)
 {  // let say 4 2 5 1 is  given array
   // at i = 0,  arr[0] = 4 and now compare entire array to get smaller element and then swap with 4;
   // we get 1 2 5 4 as new array
@@ -31,7 +31,7 @@ int main()
     {
         cin >> arr[i];
     }
-    selection(arr,n);
+    selectionsort(arr,n);
     for (int i = 0; i < n; i++)
     {
         cout<<arr[i]<<" ";
